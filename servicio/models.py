f33732bcd3f5409067821d45749576369a4ee973
Cristian Tole <toleko89@hotmail.com>
2023-06-04 18:55:47 -0500

    App Servicio funciona pendiente CSS

--- servicio/models.py
+++ servicio/models.py
@@ -8,7 +8,7 @@ from tinymce.models import HTMLField
 class Servicio(models.Model):
     titulo=models.CharField(max_length=50)
     contenido=HTMLField()
-    imagen=models.ImageField(upload_to='servicio/media')
+    imagen=models.ImageField(upload_to='servicio')
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now_add=True)
 
